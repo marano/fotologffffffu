a340d362600e78f94a0fb6b50af78fd8584e6bc8
marano <thiagomarano@gmail.com>
2010-09-21 00:53:55 -0300

    added nokogiri and fakeweb

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'haml'
+gem 'nokogiri'
 
 group :test do
   gem 'rspec', '>= 2.0.0.beta.17'
   gem 'rack-test'
+  gem 'fakeweb'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,21 @@ require 'fotolog_parser'
 
 require 'sinatra'
 require 'rack/test'
-
+require 'fakeweb'
 set :environment, :test
 
 set :views => File.join(File.dirname(__FILE__), "..", "views")
 
 @@browser = Rack::Test::Session.new(Rack::MockSession.new(Sinatra::Application))
 
+
+FakeWeb.allow_net_connect = false
+
 def browser
   @@browser
+end
+
+def fixture_file(file)
+  File.open(File.join(File.dirname(__FILE__), 'fixtures', file), 'r:utf-8').read
 end
 
