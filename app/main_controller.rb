592397bb45d898307566d31fd0586b41b12f8bba
marano <thiagomarano@gmail.com>
2010-09-21 02:14:20 -0300

    All working TESTS ARE MISSING

--- app/main_controller.rb
+++ app/main_controller.rb
@@ -1,4 +1,11 @@
+@@user = 'lunks'
 get '/' do
+  fotolog = Fotolog.new(@@user)
+  #@photos = fotolog.years.collect { |year| (1..12).collect { |month| fotolog.photos_for_month year, month } }
+  #@photos.each do |each_photo|
+    
+  #end
+  @photos = fotolog.years.map { |year| (1..12).map { |month| fotolog.photos_for_month year, month}}.flatten
   haml :index
 end
 
--- spec/main_controller_spec.rb
+++ spec/main_controller_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe 'main controller' do
+  before(:each) { browser.get '/' }
   it 'should be ok' do
-    browser.get '/'
-    puts browser.last_response.inspect
     browser.last_response.should be_ok
+  end
+  it 'should retrieve a list of photos' do
+    photos = ["http://sp4.fotolog.com.br/photo/20/29/104/marano/1220844280164_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221058240106_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221348619989_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221433699200_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221691789949_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1222477956299_f.jpg"]
+    photos.each {|photo| browser.last_response.body.match(photo).should be_true }
   end
 end
 
