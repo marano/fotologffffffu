ec689f6cb5b5edf4cfc6fe4b24938652e1d51b12
marano <thiagomarano@gmail.com>
2010-09-21 01:30:43 -0300

    photo url retrieving

--- app/fotolog.rb
+++ app/fotolog.rb
@@ -1,12 +1,27 @@
 class Fotolog
 
-  def get_archive_url user
-    "http://www.fotolog.com/#{user}/archive"
+  def initialize user
+    @user = user
   end
   
-  def get_years url
-    doc = Nokogiri::HTML(open(url))
+  def year_archive_url year
+    "http://www.fotolog.com.br/#{@user}/archive?year=#{year}&month=1"
+  end
+  
+  def years
+    doc = Nokogiri::HTML(open(archive_url))
     years = doc.css('#years').text
     years.scan(/\d{4}+/)
+  end
+
+  def archive_url
+    "http://www.fotolog.com/#{@user}/archive"
+  end
+  
+  def photos_for_month year, month
+    doc = Nokogiri::HTML(open("http://www.fotolog.com.br/#{@user}/archive?year=#{year}&month=#{month}"))
+    doc.css('.imageContainer img').map do |photo|
+      photo.attributes['src'].value.gsub('_t', '_f')
+    end
   end
 end
--- spec/fotolog_spec.rb
+++ spec/fotolog_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
 describe 'Fotolog class' do
-  let(:fotolog) {Fotolog.new}
+  let(:fotolog) { Fotolog.new 'marano' }
   context 'retrieving archive information' do
     before(:each) do
       FakeWeb.register_uri(:get, "http://www.fotolog.com/marano/archive", :body => fixture_file('archive.html'))
-
     end
     it 'should retrieve user archive URL' do
-      fotolog.get_archive_url('marano').should == 'http://www.fotolog.com/marano/archive'
+      fotolog.archive_url.should == 'http://www.fotolog.com/marano/archive'
+    end
+    it 'should retrieve year url' do
+      fotolog.year_archive_url('2004').should == 'http://www.fotolog.com.br/marano/archive?year=2004&month=1'
+    end    
+    it 'should retrieve all years which have a photo on it' do
+      years = fotolog.years
+      years.should == ['2004', '2005', '2006', '2007', '2008', '2009', '2010']
+    end
+  end
+  
+  context 'retrieving photos' do
+    before(:each) do
+      FakeWeb.register_uri(:get, "http://www.fotolog.com.br/marano/archive?year=2008&month=09", :body => fixture_file('photos.html'))
     end
     
-    it 'should retrieve all years which have a photo on it' do
-      years = fotolog.get_years 'http://www.fotolog.com/marano/archive'
-      years.should == ['2004', '2005', '2006', '2007', '2008', '2009', '2010']
+    it 'should retrieve photo urls' do
+      photos = ["http://sp4.fotolog.com.br/photo/20/29/104/marano/1220844280164_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221058240106_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221348619989_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221433699200_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221691789949_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1222477956299_f.jpg"]
+
+      fotolog.photos_for_month('2008', '09').should == photos
     end
   end
 end
