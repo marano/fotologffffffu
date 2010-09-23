f4c4379c5e4520ab75556efc3ebfe4fdc99e866e
Pedro Nascimento <pnascimento@gmail.com>
2010-09-22 22:30:00 -0300

    changed order

--- fotologffffffu.rb
+++ fotologffffffu.rb
@@ -5,9 +5,8 @@ Bundler.setup
 Bundler.require :default
 require 'open-uri'
 require 'sinatra/cache'
-set :cache_enabled, true
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
-
+set :cache_enabled, true
 Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each {|f| require f}
 
