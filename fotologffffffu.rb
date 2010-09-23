0794b73b0f49e1dfb0d206b5560e9019f06ba27a
Pedro Nascimento <pnascimento@gmail.com>
2010-09-22 22:32:36 -0300

    revert

--- fotologffffffu.rb
+++ fotologffffffu.rb
@@ -4,9 +4,8 @@ require 'bundler'
 Bundler.setup
 Bundler.require :default
 require 'open-uri'
-require 'sinatra/cache'
+
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
-set :cache_enabled, true
 Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each {|f| require f}
 
