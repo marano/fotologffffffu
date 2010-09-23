bade492fafa81717970d7310a1b74440dfa8af62
Pedro Nascimento <pnascimento@gmail.com>
2010-09-23 02:32:01 -0300

    photo url parsing bug found, a test has been created

--- app/fotolog.rb
+++ app/fotolog.rb
@@ -3,11 +3,11 @@ class Fotolog
   def initialize user
     @user = user
   end
-  
+
   def year_archive_url year
     "http://www.fotolog.com.br/#{@user}/archive?year=#{year}&month=1"
   end
-  
+
   def years
     doc = Nokogiri::HTML(open(archive_url))
     years = doc.css('#years').text
@@ -17,15 +17,20 @@ class Fotolog
   def archive_url
     "http://www.fotolog.com/#{@user}/archive"
   end
-  
+
   def photos
     years.map { |year| (1..12).map { |month| photos_for_month year, month}}.flatten
   end
-  
+
   def photos_for_month year, month
     doc = Nokogiri::HTML(open("http://www.fotolog.com.br/#{@user}/archive?year=#{year}&month=#{'0' if month.to_i < 10}#{month.to_i}"))
     doc.css('.imageContainer img').map do |photo|
-      photo.attributes['src'].value.gsub('_t', '_f')
+      full_image_for photo.attributes['src'].value
     end
   end
+
+  def full_image_for photo
+    photo.gsub('_t', '_f')
+  end
 end
+
--- spec/fotolog_spec.rb
+++ spec/fotolog_spec.rb
@@ -11,22 +11,27 @@ describe 'Fotolog class' do
     end
     it 'should retrieve year url' do
       fotolog.year_archive_url('2004').should == 'http://www.fotolog.com.br/marano/archive?year=2004&month=1'
-    end    
+    end
     it 'should retrieve all years which have a photo on it' do
       years = fotolog.years
       years.should == ['2004', '2005', '2006', '2007', '2008', '2009', '2010']
     end
   end
-  
+
   context 'retrieving photos' do
     before(:each) do
       FakeWeb.register_uri(:get, "http://www.fotolog.com.br/marano/archive?year=2008&month=09", :body => fixture_file('photos.html'))
     end
-    
+
     it 'should retrieve photo urls' do
       photos = ["http://sp4.fotolog.com.br/photo/20/29/104/marano/1220844280164_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221058240106_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221348619989_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221433699200_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1221691789949_f.jpg", "http://sp4.fotolog.com.br/photo/20/29/104/marano/1222477956299_f.jpg"]
-
       fotolog.photos_for_month('2008', '09').should == photos
+    end
+    pending 'should parse photo url correctly' do
+      photo = 'http://spc.fotolog.com.br/photo/12/16/103/pink_that/1189983175_t.jpg'
+      big_photo = 'http://spc.fotolog.com.br/photo/12/16/103/pink_that/1189983175_f.jpg'
+      fotolog.full_image_for(photo).should == big_photo
     end
   end
 end
+
