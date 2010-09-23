d0e928511767a543b7ae4853e6d7125a833dfb76
Pedro Nascimento <pnascimento@gmail.com>
2010-09-22 22:28:00 -0300

    Implemented caching (hopefully)

--- fotologffffffu.rb
+++ fotologffffffu.rb
@@ -1,16 +1,13 @@
 require 'rubygems'
 require 'bundler'
- 
+
 Bundler.setup
 Bundler.require :default
 require 'open-uri'
-
-
+require 'sinatra/cache'
+set :cache_enabled, true
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
-#set :config_path, File.join(Sinatra::Application.root, 'config')
-
-#require File.join(Sinatra::Application.config_path, 'config.rb')
 
 Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each {|f| require f}
 
