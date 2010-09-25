2ed1115e73d5eab0dead4d505f052a14797082a1
Lunks <lunks@lunks-laptop.(none)>
2010-09-25 16:32:49 -0300

    mocked fotolog pages now can handle a different username

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,16 +17,16 @@ def fixture_file(file)
   File.open(File.join(File.dirname(__FILE__), 'fixtures', file), 'r:utf-8').read
 end
 
-def mock_requests_fixture_for_fotolog
-  FakeWeb.register_uri(:get, "http://www.fotolog.com/marano/archive", :body => fixture_file('archive.html'))
-  
+def mock_requests_fixture_for_fotolog name
+  FakeWeb.register_uri(:get, "http://www.fotolog.com/#{name}/archive", :body => fixture_file('archive.html'))
+
   (2003..2010).to_a.each do |year|
     (1..12).to_a.each do |month|
-      FakeWeb.register_uri(:get, "http://www.fotolog.com.br/marano/archive?year=#{year}&month=#{'0' if month.to_i < 10}#{month}", :body => fixture_file('photos.html'))
+      FakeWeb.register_uri(:get, "http://www.fotolog.com.br/#{name}/archive?year=#{year}&month=#{'0' if month.to_i < 10}#{month}", :body => fixture_file('photos.html'))
     end
   end
-  
-  FakeWeb.register_uri(:get, "http://www.fotolog.com.br/marano/archive?year=2008&month=09", :body => fixture_file('photos.html'))
+
+  FakeWeb.register_uri(:get, "http://www.fotolog.com.br/#{name}/archive?year=2008&month=09", :body => fixture_file('photos.html'))
 end
 
 Cache.stubs(:instance).returns(CacheStub.new)
